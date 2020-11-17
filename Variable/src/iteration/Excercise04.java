3e4b5d2887b5a6b18d5b188c6f342bc088f4ccf0
admin <admin@YD03-09>
2020-11-17 12:40:26 +0900

    배열숙제

--- Variable/src/iteration/Excercise04.java
+++ Variable/src/iteration/Excercise04.java
@@ -3,8 +3,20 @@ package iteration;
 public class Excercise04 {
 
 	public static void main(String[] args) {
-		
+		int sum = 0;
+		int i1 = 0;
+		int i2 = 1;
+		boolean run = true;
+		while (run) {
+			i1 = (int) (Math.random() * 6) + 1;
+			i2 = (int) (Math.random() * 6) + 1;
+			sum = i1 + i2;
+			{
+				System.out.println(i1 + "," + i2);
+				if (sum == 5)
+					break;
+			}
 
+		}
 	}
-
 }
